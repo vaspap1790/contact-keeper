537401b34be3238260b91c8284cc2e3911619f79
vaspap1790 <vaspap1790@gmail.com>
2021-02-25 18:05:12 +0200

    server set up

---
+++ server.js
@@ -0,0 +1,10 @@
+const e = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json({msg:'Welcome to Contact Keeper API!'}))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
