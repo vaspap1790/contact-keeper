8769de9c5e83bf00937b8e6fd3cc074770627d6b
vaspap1790 <vaspap1790@gmail.com>
2021-02-28 19:36:31 +0200

    logout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Fragment, useContext} from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
 
 const Navbar = ({ title,icon }) => {
-    return (
-        <div className="navbar bg-primary">
-            <h1>
-                <i className={icon} /> {title}
-            </h1>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
+
+    const authContext = useContext(AuthContext);
+    const { isAuthenticated, logout, user } = authContext;
+
+    const onLogout = () => {
+        logout();
+    }
+
+    const authLinks = (
+        <Fragment>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/about'>About</Link>
+            </li>
+            <li> <em> Hello { user && user.name } </em> </li>
+            <li>
+                <a onClick={onLogout} href="#!">
+                    <i className="fas fa-sign-out-alt"></i> <span className="hide-sm">Logout</span>
+                </a>
+            </li>
+        </Fragment>
+    );
+    const guestLinks = (
+        <Fragment>
                 <li>
                     <Link to='/about'>About</Link>
                 </li>
@@ -21,9 +39,16 @@ const Navbar = ({ title,icon }) => {
                 <li>
                     <Link to='/login'>Login</Link>
                 </li>
-                <li>
-                    <Link to='/logout'>Logout</Link>
-                </li>
+        </Fragment>
+    );
+
+    return (
+        <div className="navbar bg-primary">
+            <h1>
+                <i className={icon} /> {title}
+            </h1>
+            <ul>
+                {isAuthenticated ? authLinks : guestLinks}
             </ul>
         </div>
     )
